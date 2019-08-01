4aa3712a0a88d7d5568dac838abc60ab6c586338
German Viscuso <germanviscuso@gmail.com>
2019-08-01 19:16:17 +0200

    Added more debugging messages

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -210,6 +210,7 @@ const UpsellBuyResponseHandler = {
         let preSpeechText;
 
         // check the Buy status - accepted, declined, already purchased, or something went wrong.
+        console.log(handlerInput.requestEnvelope.request.name + ' connections result: ' + handlerInput.requestEnvelope.request.payload.purchaseResult);
         switch (handlerInput.requestEnvelope.request.payload.purchaseResult) {
           case 'ACCEPTED':
           case 'ALREADY_PURCHASED':
@@ -321,6 +322,7 @@ const CancelProductResponseHandler = {
     let speechText;
     let repromptOutput;
 
+    console.log('Cancel connections result: ' + handlerInput.requestEnvelope.request.payload.purchaseResult);
     return monetizationClient.getInSkillProducts(locale).then((res) => {
       const product = res.inSkillProducts.filter(
         record => record.productId === productId,
